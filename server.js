1f3bdf088ee27226a6246301cb290e791ad04757
creativebcx <ericdonley1@gmail.com>
2017-06-19 14:04:22 -0600

    server-start-updates

--- server.js
+++ server.js
@@ -1,86 +1,63 @@
-
 const express = require('express');
-const router = express.Router();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-
-const {BlogPosts} = require('./models');
-
-const jsonParser = bodyParser.json();
 const app = express();
+const morgan = require('morgan');
 
-// log the http layer
+const blogPostsRouter = require('./blogPostsRouter');
+
 app.use(morgan('common'));
-
-//add data to BlogPosts
-//create: function(title, content, author, publishDate)
-BlogPosts.create('The Weather', 'It is sunny today', 'Eric', '6/13/17');
-BlogPosts.create('Food', 'Pasta is yummy', 'EricD', '6/14/2017');
-
-// when the root of this router is called with GET, return
-// all current ShoppingList items
-app.get('/blog-post', (req, res) => {
-  res.json(BlogPosts.get());
-});
-
-app.post('/blog-post', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
-  const requiredFields = ['title', 'content', 'author', 'publishDate'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-
-  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
-  res.status(201).json(item);
-});
-
-//test
-// when PUT request comes in with updated item, ensure has
-// required fields. also ensure that item id in url path, and
-// item id in updated item object match. if problems with any
-// of that, log error and send back status code 400. otherwise
-// call `ShoppingList.update` with updated item.
-app.put('/blog-post/:id', jsonParser, (req, res) => {
-  const requiredFields = ['title', 'content', 'author', 'publishDate'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  if (req.params.id !== req.body.id) {
-    const message = (
-      `Request path id (${req.params.id}) and request body id `
-      `(${req.body.id}) must match`);
-    console.error(message);
-    return res.status(400).send(message);
-  }
-  console.log(`Updating blog post \`${req.params.id}\``);
-  BlogPosts.update({
-    id: req.params.id,
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author,
-    publishDate: req.body.author
-  });
-  res.status(204).end();
-});
-
-// when DELETE request comes in with an id in path,
-// try to delete that item from ShoppingList.
-app.delete('/blog-post/:id', (req, res) => {
-  BlogPosts.delete(req.params.id);
-  console.log(`Deleted blog post \`${req.params.ID}\``);
-  res.status(204).end();
-});
+/* old listening function
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
+
+*/
+
+// blog posts router
+app.use('/blog-posts', blogPostRouter);
+
+let server;
+
+// run server function
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log('Your app os listening on port ${port}');
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();    
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test/test-server.js
@@ -0,0 +1,44 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('blog-post', function() {
+  
+  before(function() {
+    return runServer();
+  });
+
+  // Close server after these tests run in case
+  // we have other test modules that need to 
+  // call `runServer`. If server is already running,
+  // `runServer` will error out.
+  after(function() {
+    return closeServer();
+  });
+  // `chai.request.get` is an asynchronous operation. When
+  // using Mocha with async operations, we need to either
+  // return an ES6 promise or else pass a `done` callback to the
+  // test that we call at the end. We prefer the first approach, so
+  // we just return the chained `chai.request.get` object.
+  it('should list users on GET', function() {
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.be.above(0);
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.have.all.keys(
+            'id', 'title', 'content', 'author', 'publishDate');
+        });
+      });
+  });
+});
+
